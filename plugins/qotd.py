7632d5176a9133ca4c1a971efcd847d22f1fa2a4
jha <jha@9307e528-e534-0410-a844-a96db09f00f2>
2007-07-11 21:55:45 +0000

    tabs > spaces
    git-svn-id: http://stalverk80.se/svn/pynik@11 9307e528-e534-0410-a844-a96db09f00f2

--- plugins/qotd.py
+++ plugins/qotd.py
@@ -5,97 +5,97 @@ from commands import Command
 import string, random
 
 class quote:
-	quote = ""
-	index = 0
-	played = 0
+    quote = ""
+    index = 0
+    played = 0
 
 class QuoteCollection:
-	quotes = []
-	plays = []
-	quotefilename = "data/quotes.txt"
-	playlistfilename = "data/playlist.txt"
-	
-	def __init__(self):
-		self.LoadFromFiles()
-	
-	def GetQuote(self):
-			print 'GetQuote'
-			self.NormalizePlays()
-			DueQuotes = self.GetDueQuotes()
-			if DueQuotes:
-				chosenQuote = random.choice(DueQuotes)
-				self.quotes[chosenQuote.index].played += 1
-	   			
-			 	output = '#%s: %s' % (self.quotes[chosenQuote.index].index, self.quotes[chosenQuote.index].quote)
-				print  "choosing #%s out of %s quotes: %s (played %s times)" % (chosenQuote.index, len(DueQuotes), chosenQuote.quote, chosenQuote.played)
-				return output
-			else:
-				print "emtpy list"
+    quotes = []
+    plays = []
+    quotefilename = "data/quotes.txt"
+    playlistfilename = "data/playlist.txt"
+    
+    def __init__(self):
+        self.LoadFromFiles()
+    
+    def GetQuote(self):
+            print 'GetQuote'
+            self.NormalizePlays()
+            DueQuotes = self.GetDueQuotes()
+            if DueQuotes:
+                chosenQuote = random.choice(DueQuotes)
+                self.quotes[chosenQuote.index].played += 1
+                   
+                output = '#%s: %s' % (self.quotes[chosenQuote.index].index, self.quotes[chosenQuote.index].quote)
+                print  "choosing #%s out of %s quotes: %s (played %s times)" % (chosenQuote.index, len(DueQuotes), chosenQuote.quote, chosenQuote.played)
+                return output
+            else:
+                print "emtpy list"
 
-	def NormalizePlays(self):
-		plays = []
+    def NormalizePlays(self):
+        plays = []
 
-		for quote in self.quotes:
-			plays.append(quote.played)
-		minplays = int(min(plays))
-		if minplays != 0:
-		   for quote in self.quotes:
-			   quote.played = int(quote.played) - int(minplays)
+        for quote in self.quotes:
+            plays.append(quote.played)
+        minplays = int(min(plays))
+        if minplays != 0:
+           for quote in self.quotes:
+               quote.played = int(quote.played) - int(minplays)
 
-	def GetDueQuotes(self):
-		for quote in self.quotes:
-			print quote.played
+    def GetDueQuotes(self):
+        for quote in self.quotes:
+            print quote.played
 
-		DueQuotes = []
-		for quote in self.quotes:
-			if quote.played == 0:
-				DueQuotes.append(quote)
-		return DueQuotes;
+        DueQuotes = []
+        for quote in self.quotes:
+            if quote.played == 0:
+                DueQuotes.append(quote)
+        return DueQuotes;
 
-	def LoadFromFiles(self):
-		indexCounter = 1
-		playlistfile = open(self.playlistfilename, 'r') 
-		with open(self.quotefilename, 'r') as quotefile:
-			for line in quotefile:
-				currentQuote = quote()
+    def LoadFromFiles(self):
+        indexCounter = 1
+        playlistfile = open(self.playlistfilename, 'r') 
+        with open(self.quotefilename, 'r') as quotefile:
+            for line in quotefile:
+                currentQuote = quote()
 
-				currentQuote.quote = string.strip(line)
-			  	played = playlistfile.readline()
-			  	try:
-					currentQuote.played = int(played)
-			  	except:
-					currentQuote.played = 0
-			  	currentQuote.index = indexCounter
-			  
-				self.quotes.append(currentQuote)
-				indexCounter += 1
+                currentQuote.quote = string.strip(line)
+                played = playlistfile.readline()
+                try:
+                    currentQuote.played = int(played)
+                except:
+                    currentQuote.played = 0
+                currentQuote.index = indexCounter
+              
+                self.quotes.append(currentQuote)
+                indexCounter += 1
 
-	def SaveToFiles(self):
-		quotefile = open(self.quotefilename, "w")
-		playlistfile = open(self.playlistfilename, "w")
-		quotesToWrite = []
-		playlistToWrite = [] 
-		for quote in self.quotes:
-			quotesToWrite.append(quote.quote + "\n")
-			playlistToWrite.append(str(quote.played) + "\n")
-		
-		quotefile.writelines(quotesToWrite)
-		playlistfile.writelines(playlistToWrite)
-		
-		quotefile.close()
-		playlistfile.close()
+    def SaveToFiles(self):
+        quotefile = open(self.quotefilename, "w")
+        playlistfile = open(self.playlistfilename, "w")
+        quotesToWrite = []
+        playlistToWrite = [] 
+        for quote in self.quotes:
+            quotesToWrite.append(quote.quote + "\n")
+            playlistToWrite.append(str(quote.played) + "\n")
+        
+        quotefile.writelines(quotesToWrite)
+        playlistfile.writelines(playlistToWrite)
+        
+        quotefile.close()
+        playlistfile.close()
 
 
 
 class QuoteCommand(Command):
-	triggers = ['qotd']
-	collection = QuoteCollection()
+    triggers = ['qotd']
+    collection = QuoteCollection()
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def on_trigger(self, bot, source, target, trigger, argument):
-		self.on_qotd(bot, source, target, trigger, argument)
+    def on_trigger(self, bot, source, target, trigger, argument):
+        self.on_qotd(bot, source, target, trigger, argument)
 
-	def on_qotd(self, bot, source, target, trigger, argument):
-		bot.tell(target, self.collection.GetQuote())
+    def on_qotd(self, bot, source, target, trigger, argument):
+        bot.tell(target, self.collection.GetQuote())
