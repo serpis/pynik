601928ab4eaf03867617e0a1f1dfeb2a23915cd0
jaf <jaf@9307e528-e534-0410-a844-a96db09f00f2>
2007-07-17 13:49:38 +0000

    git-svn-id: http://stalverk80.se/svn/pynik@18 9307e528-e534-0410-a844-a96db09f00f2

--- plugins/example_command.py
+++ plugins/example_command.py
@@ -10,3 +10,8 @@ class ExampleCommand(Command):
 		if target[0] == '#': # is the first character in target a #? then, it's a channel!
 			pass
 			#bot.tell(target, "Ohayou %s! %s is a channel with %s users: %s!" % (source, target, len(bot.nick_lists[target]), bot.nick_lists[target]))
+
+
+command = ExampleCommand()
+
+command.trig_example(None, 'dk', '#c++.se', 'example', 'huh?')
