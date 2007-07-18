4e128b60cff5667eb2ac6e76472a6e5c065ca8dd
jaf <jaf@9307e528-e534-0410-a844-a96db09f00f2>
2007-07-18 10:38:11 +0000

    git-svn-id: http://stalverk80.se/svn/pynik@26 9307e528-e534-0410-a844-a96db09f00f2

--- plugins/title_reader.py
+++ plugins/title_reader.py
@@ -5,14 +5,10 @@ from commands import Command
 
 class TitleReaderPlugin(Command): 
 	hooks = ['on_privmsg']   
-	black_urls = []
 	last_url = {}
 
 	def __init__(self):
 		pass
-	
-	def get_options(self):
-		return ['black_urls']
 	
 	def on_privmsg(self, bot, source, target, tupels):
 		message = tupels[5]
@@ -39,30 +35,3 @@ class TitleReaderPlugin(Command):
 				bot.tell(target, 'I can\'t find a title for ' + self.last_url[target]) 
 		else:
 			bot.tell(target, 'I haven\'t seen any urls here yet.')
-
-	def on_load(self):
-		del self.black_urls[:]
-
-		file = open('data/black_urls.txt', 'r')
-
-		while True:
-			line = file.readline()
-			if not line:
-				break
-
-			m = re.match('^(.+)$', line)
-			
-			if m:
-				self.black_urls.append(m.group(1))
-
-	def save(self):
-		file = open('data/black_urls.txt', 'w')
-
-		for url in self.black_urls:
-			file.write(url)
-			file.write('\n')
-
-		file.close()
-
-	def on_modified_options(self):
-		self.save()
