a8ab205eb3e4d0705a9a2aabebd1e12197fdef10
dkg <dkg@9307e528-e534-0410-a844-a96db09f00f2>
2007-07-18 16:55:43 +0000

    git-svn-id: http://stalverk80.se/svn/pynik@28 9307e528-e534-0410-a844-a96db09f00f2

--- plugins/title_reader.py
+++ plugins/title_reader.py
@@ -1,14 +1,50 @@
+from __future__ import with_statement
+import pickle
 import sys
 import re
+import utility
 from plugins import Plugin
 from commands import Command
 
+class URL():
+	url = ''
+	title = ''
+	timestamp = ''
+	nick = ''
+	def is_match(self, searchword):
+		if re.search(searchword, self.url):
+			return True
+		if re.search(searchword, self.title):
+			return True
+		if re.search(searchword, self.nick):
+			return True
+		return False
+	
+
 class TitleReaderPlugin(Command): 
 	hooks = ['on_privmsg']   
-	last_url = {}
+	black_urls = []
+	urls = {}
 
 	def __init__(self):
 		pass
+	
+	def get_options(self):
+		return ['black_urls']
+
+	def set_title(self, target):
+		import urllib
+		if not re.search('http', self.urls[target].url):
+			self.urls[target].url = 'http://' + self.urls[target].url
+		data = urllib.urlopen(self.urls[target].url).read()
+
+		m = re.search('<title>(.+?)<\/title>', data, re.IGNORECASE)
+
+		if m:
+			self.urls[target].title = m.group(1)
+			self.urls[target].title = re.sub('<.+?>', '', self.urls[target].title)
+		else:
+			self.urls[target].title = None
 	
 	def on_privmsg(self, bot, source, target, tupels):
 		message = tupels[5]
@@ -16,23 +52,96 @@ class TitleReaderPlugin(Command):
 		m = re.search('((http:\/\/|www.)\S+)', message)
 
 		if m:
-			self.last_url[target] = m.group(1)
+			if target not in self.urls.keys():			
+				self.urls[target] = URL()
+
+			self.urls[target].url = m.group(1)
+			self.urls[target].nick = source
+			self.urls[target].timestamp = 'test'
+			self.set_title(target)
+			self.save_last_url(target)
+
+	def save_last_url(self, target):
+		list = []
+		try:
+			file = open('data/' + target + '-urls.txt', 'r')
+			list = pickle.Unpickler(file).load()
+			file.close()
+		except IOError:
+			pass
+
+		file = open('data/' + target + '-urls.txt', 'w')
+
+		list.append(self.urls[target])
+		p = pickle.Pickler(file)
+		p.dump(list)
+
+		file.close()
+
+	def trig_urlsearch(self, bot, source, target, trigger, argument):
+		list = []
+		resultlist = []
+		match = False
+
+		searchlist = argument.split(' ')
+		try:
+			file = open('data/' + target + '-urls.txt', 'r')
+			list = pickle.Unpickler(file).load()
+			file.close()
+
+			for object in list:
+				match = True
+				for word in searchlist:
+					if not object.is_match(word):
+						match = False
+						break
+				if match:
+					resultlist.append(object)
+
+			if len(resultlist) > 0:
+				print(target, 'Match: ' + resultlist[0].url)
+			else:
+				print(target, 'No match found')
+
+		except IOError:
+			print(target, 'I have no urls for this channel')
+
+		
 
 	def trig_title(self, bot, source, target, trigger, argument):
-		
-		if target in self.last_url.keys():
-			import urllib
-		
-			data = urllib.urlopen(self.last_url[target]).read()
-
-			m = re.search('<title>\s*(.+?)\s*<\/title>', data, re.IGNORECASE)
+		if target in self.urls.keys():
+			m = self.urls[target].title
 
 			if m:
-				title = m.group(1)
-				title = re.sub('<.+?>', '', title)
-
-				bot.tell(target, title)
+				print(target, m)
 			else:
-				bot.tell(target, 'I can\'t find a title for ' + self.last_url[target]) 
+				print(target, 'I can\'t find a title for ' + self.urls[target].url) 
 		else:
-			bot.tell(target, 'I haven\'t seen any urls here yet.')
+			print(target, 'I haven\'t seen any urls here yet.')
+
+	def on_load(self):
+		del self.black_urls[:]
+
+		file = open('data/black_urls.txt', 'r')
+
+		while True:
+			line = file.readline()
+			if not line:
+				break
+
+			m = re.match('^(.+)$', line)
+			
+			if m:
+				self.black_urls.append(m.group(1))
+
+	def save(self):
+		file = open('data/black_urls.txt', 'w')
+
+		for url in self.black_urls:
+			file.write(url)
+			file.write('\n')
+
+		file.close()
+
+	def on_modified_options(self):
+		self.save()
