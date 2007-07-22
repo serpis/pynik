e7ba5e4b605e165434f80793774752537466f114
jaf <jaf@9307e528-e534-0410-a844-a96db09f00f2>
2007-07-22 00:43:35 +0000

    git-svn-id: http://stalverk80.se/svn/pynik@40 9307e528-e534-0410-a844-a96db09f00f2

---
+++ plugins/teewars.py
@@ -0,0 +1,36 @@
+# coding: latin-1
+
+from __future__ import with_statement
+from commands import Command
+import re
+from socket import *
+
+class TeewarsCommand(Command):
+	def __init__(self):
+		pass
+
+	def trig_teewars(self, bot, source, target, trigger, argument):
+		address = "jp.serp.se"
+		port = 8303
+
+		m = re.search("^(\S+):?(\d*)", argument)
+
+		if m:
+			address = m.groups(1)
+			if m.groups(2):
+				port = int(m.groups(1))
+			
+
+		sock = socket(AF_INET, SOCK_DRAM)
+		sock.sendto("\xff\xff\xff\xff\xff\xff\xff\xff\xff\xffgief", (address, port))
+		data, addr = sock.recvfrom(1024)
+
+		data = data[14:]
+
+		server_name, map_name = data[:-2].split("\x00")[0:2]
+		data = data[-2:]
+		max_players = ord(data[0])
+		num_players = ord(data[1])
+
+		bot.tell(target, "Server '%s' at %s:%s has %s/%s players." % (server_name, address, port, num_players, max_players))
+
