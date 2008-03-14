5366c208698846985b8a093058b96e6392944fe4
jsj <jsj@9307e528-e534-0410-a844-a96db09f00f2>
2008-03-14 10:18:51 +0000

    init också
    git-svn-id: http://stalverk80.se/svn/pynik@66 9307e528-e534-0410-a844-a96db09f00f2

--- plugins/pi.py
+++ plugins/pi.py
@@ -7,15 +7,18 @@ pi = "3.141592653589793238462643383279"\
 import re
 import utility
 from commands import Command
-				
-class picomp(Command):
-	def __init__(self):
-		pass
-	
-	def trig_pi(self, bot, source, target, trigger, argument):
-		argumet = argument.strip()
+
+def control_pi(argument):
+	argumet = argument.strip()
 		if len(argument) > 50:
 			return "Copypasta is FTL!"
 		if argument == pi[:len(argument)]:
 			return "Congratulations, you know pi to %s decimals" % len(argument)-4
 		return "No no, %s isn't pi,try again, truncate this time" % argument
+
+class picomp(Command):
+	def __init__(self):
+		pass
+	
+	def trig_pi(self, bot, source, target, trigger, argument):
+		return control_pi(argument)
