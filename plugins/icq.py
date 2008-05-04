dd4a73af20b6bedce62bfe8eb0e7435f2d1bf95f
tee <tee@9307e528-e534-0410-a844-a96db09f00f2>
2008-05-04 19:08:20 +0000

    First checkin of ICQ plug-in
    git-svn-id: http://stalverk80.se/svn/pynik@87 9307e528-e534-0410-a844-a96db09f00f2

---
+++ plugins/icq.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+
+import re
+import utility
+from commands import Command
+
+def icq_lookup(icqid):
+	url = 'http://www.icq.com/people/about_me.php?uin=' + utility.escape(icqid)
+	response = utility.read_url(url)
+	data = response["data"]
+
+	search = re.search('<div class="uinf-2-2-2-1">(.*?)</div>.*?<div class="uinf-2-2-2-2">(.*?)</div>.*?<div class="uinf-2-2-2-4">(.*?)</div>.*?<div class="uinf-2-2-2-4">(.*?)</div>', data)
+
+	if search:
+		nick = search.group(1)
+		info = search.group(2)
+		if info
+			info = re.sub("\n|\r\n|\n\n",", ",info
+		city = search.group(3)
+		country = search.group(4)
+		
+		if nick:
+			result = nick
+		if info or city or country:
+			result = result + ": "
+			
+		if info:
+			result = result + info
+		if city:
+			result = result + ", " + city
+		if country:
+			result = result + ", " + country
+		return result
+
+		else:
+			return None
+	else:
+		return None
+				
+class icq(Command):
+	def __init__(self):
+		pass
+	
+	def trig_icq(self, bot, source, target, trigger, argument):
+		
+		result = icq_lookup(argument)
+
+		if result == None:
+			return "No result returned."
+
+		fixedline = re.sub("\s+"," ", result)
+		
+		return fixedline
