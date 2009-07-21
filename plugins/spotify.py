4d19cecf227e47f13831e3ad725f17a68f478073
tee <tee@9307e528-e534-0410-a844-a96db09f00f2>
2009-07-21 22:51:36 +0000

    .spotify now does passive mode as well.
    git-svn-id: http://stalverk80.se/svn/pynik@123 9307e528-e534-0410-a844-a96db09f00f2

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -10,9 +10,16 @@ from commands import Command
 import command_catcher
 
 class Spot(object):
-	def __init__(self, protocol, spotify_resource):
-		self.prot = protocol
-		self.spot = spotify_resource
+	def __init__(self, type, hash, format):
+		self.type = type
+		self.hash = hash
+		self.format = format
+
+	def URI(self):
+		return "spotify:%s:%s" % (type, hash)
+
+	def URL(self):
+		return "http://open.spotify.com/%s/%s" % (type, hash)
 
 class SpotifyConvertPlugin(Command):
 	hooks = ['on_privmsg']
@@ -23,8 +30,12 @@ class SpotifyConvertPlugin(Command):
 		pass
 
 	def spot_lookup(self, type, hash):
+		tempSpot = Spot(type, hash, 'URI')
+		return self.spot_lookup_direct(tempSpot)
 
-		url = "http://spotify.url.fi/%s/%s" % (type, hash)
+
+	def spot_lookup_direct(self, theSpot):
+		url = "http://spotify.url.fi/%s/%s" % (theSpot.type, theSpot.hash)
 		response = utility.read_url(url)
 		data = response["data"]
 
@@ -32,12 +43,16 @@ class SpotifyConvertPlugin(Command):
 
 		artist = re.search(r"<span>Artist</span>\s*<a.*?>(?P<artist>.+?)</a>", data, re.DOTALL)
 		if artist: artist = artist.group(1)
+
 		album = re.search(r"<span>Album</span>\s*<a.+?>(?P<album>.+?)</a>", data, re.DOTALL)
 		if album: album = album.group(1)
+
 		year = re.search(r"<span>Year</span>\s*(?P<year>.+?)\s*</p>", data, re.DOTALL)
 		if year: year = year.group(1)
+
 		track = re.search(r"<span>Track</span>\s*<a.+?>(?P<track>.+?)</a>", data, re.DOTALL)
 		if track: track = track.group(1)
+
 		length = re.search("<span>Length</span>\s*(?P<length>.+?)\s*</p>", data, re.DOTALL)
 		if length: length = length.group(1)
 
@@ -54,21 +69,23 @@ class SpotifyConvertPlugin(Command):
 
 		return output
 
-
 	def on_privmsg(self, bot, source, target, message):
-		m = re.search(r'http://open\.spotify\.com/track/(\w+)', message)
-		prot = 'http'
+		m = re.search(r'http://open\.spotify\.com/(?P<type>.+?)/(?P<hash>\w+)', message)
+		prot = 'URL'
 		if not m:
-			prot = 'spotify'
-			m = re.search(r'spotify:track:(\w+)', message)
+			prot = 'URI'
+			m = re.search(r'spotify:(?P<type>.+?):(?P<hash>\w+)', message)
 		if m:
-			spot = m.group(1)
-			self.spots[target] = Spot(prot, spot)
+			type = m.group('type')
+			hash = m.group('hash')
+			self.spots[target] = Spot(type, hash, prot)
 			self.save_last_spot(target)
+
 
 	def save_last_spot(self, target):
 		self.spot_list.append(self.spots[target])
 		self.save_spots()
+
 
 	def trig_spotify(self, bot, source, target, trigger, argument):
 		if argument:
@@ -81,35 +98,38 @@ class SpotifyConvertPlugin(Command):
 				return self.spot_lookup(m.group("type"), m.group("hash"))
 
 			else:
-				return "couldn't make sense of that."
+				return "Couldn't make sense of that."
 
 		elif target in self.spots.keys():
 			m = self.spots[target]
-			if m.prot == 'http':
-				return "spotify:track:%s"%m.spot
+			if m:
+				return self.spot_lookup_direct(m)
 			else:
-				return "http://open.spotify.com/track/%s"%m.spot
-		else:
-			return 'I haven\'t seen any urls here yet.'
+				return 'I haven\'t seen any urls here yet.'
+
 
 	def save_spots(self):
-		file = open('data/spots.txt', 'w')
+		file = open('data/spots2.txt', 'w')
 		p = pickle.Pickler(file)
 		p.dump(self.spot_list)
 		file.close()
 
+
 	def load_spots(self):
 		try:
-			with open('data/spots.txt', 'r') as file:
+			with open('data/spots2.txt', 'r') as file:
 				self.spot_list = pickle.Unpickler(file).load()
 		except IOError:
 			pass
 
+
 	def on_load(self):
 		self.load_spots()
 
+
 	def save(self):
 		pass
+
 
 	def on_modified_options(self):
 		self.save()
