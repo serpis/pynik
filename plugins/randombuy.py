6c96e961b472d8a8d6f752ec8c334b9bd7ae948a
merola <merola@9307e528-e534-0410-a844-a96db09f00f2>
2009-10-23 21:54:15 +0000

    Some upgrades in randombuy, hopefully works :P
    git-svn-id: http://stalverk80.se/svn/pynik@206 9307e528-e534-0410-a844-a96db09f00f2

--- plugins/randombuy.py
+++ plugins/randombuy.py
@@ -6,9 +6,9 @@ import re
 import utility
 from commands import Command
 
-def random_product_dealextreme(max_price, hardcore):
-	max_dollars = utility.currency_conversion(int(max_price), 'sek', 'usd')
-	if max_dollars == None:
+def random_product_dealextreme(min_price, max_price, hardcore):
+	conversion_rate = utility.currency_conversion(1, 'usd', 'sek')
+	if conversion_rate == None:
 		return "Ajdå, nu gick något fel :("
 	
 	# Fetch the web page
@@ -21,16 +21,17 @@ def random_product_dealextreme(max_price, hardcore):
 	product_iterator = re.finditer(product_pattern, data)
 	
 	for match in product_iterator:
-		if float(match.group(3)) <= max_dollars:
+		cost = float(match.group(3)) * conversion_rate
+		if min_price <= cost and cost <= max_price:
 			if hardcore:
 				return "Köööööp! http://www.dealextreme.com/shoppingcart.dx/add." + match.group(1) + "~quantity.1#_ctl0_content_pCheckout"
 			else:
 				return "$" + match.group(3) + ": " + match.group(2) + " | http://www.dealextreme.com/details.dx/sku." + match.group(1)
 	
-	return "Ingen tillräckligt billig produkt hittades, öka gränsen eller försök igen!"
+	return "Ingen produkt med lagom pris hittades, ändra dina krav eller försök igen!"
 
 class RandomBuyCommand(Command):
-	usage = "Användning: .köp <maxkostnad i SEK> | Många 'ö' ger direktlänk till kassan med produkt tillagd, istället för produktdetaljer."
+	usage = "Användning: .köp <maxkostnad>|<intervall>|=<ungefärlig kostnad> | Många 'ö' ger direktlänk till kassan med produkt tillagd, istället för produktdetaljer. Kostnader anges i hela svenska kronor!"
 	
 	def __init__(self):
 		pass
@@ -47,13 +48,25 @@ class RandomBuyCommand(Command):
 		if not args[1]:
 			bot.tell(target, self.usage)
 		
-		# Ensure max price is a number
-		elif not args[1].isdigit():
-			bot.tell(target, "Maxkostnaden måste anges i hela svenska kronor!")
-			
-		# dealextreme.com
+		m = re.match(r'((\d+)-(\d+))|(\d+)|=(\d+)', args[1])
+		if m:
+			if m.group(1):
+				min_price = m.group(2)
+				max_price = m.group(3)
+			elif m.group(4):
+				min_price = 0
+				max_price = m.group(4)
+			elif m.group(5):
+				min_price = m.group(5) - 7.5
+				max_price = m.group(5) + 7.5
+			else:
+				bot.tell(target, "Ojoj, nu gick det fel igen :(")
 		else:
-			# Length varies depending on encoding, so this is not a good way to do it
-			hardcore = (len(args[0]) > 3)
-			bot.tell(target, random_product_dealextreme(args[1], hardcore))
-
+			bot.tell(target, "Hörru, så gör man inte!" + self.usage)
+			
+		# Randomize from dealextreme.com
+		
+		# Length varies depending on encoding, so this is not a good way to do it
+		hardcore = (len(args[0]) > 3)
+		bot.tell(target, random_product_dealextreme(min_price, max_price, hardcore))
+		
