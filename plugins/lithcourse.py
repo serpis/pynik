755c54a92b2a02dbc806f596ed9cf6e5c0ff6b45
merola <merola@9307e528-e534-0410-a844-a96db09f00f2>
2009-10-25 20:47:00 +0000

    LithCourse: Added advancement level.
    git-svn-id: http://stalverk80.se/svn/pynik@211 9307e528-e534-0410-a844-a96db09f00f2

--- plugins/lithcourse.py
+++ plugins/lithcourse.py
@@ -31,6 +31,91 @@ def schedule_url(code, programme, year):
 		url += "&-Token.pmk=" + programme
 	return url
 
+def lith_course_info(code, programme, year):
+	# Fetch the study handbook page for the course
+	response = utility.read_url(sh_url(code, year))
+	sh_data = response["data"]
+	
+	# Locate the Swedish course name
+	m = re.search(
+		"\<span class=\"txtbold\"\>\<b\>(.*?), \d{1,2},?\d? p \</b\>",
+		sh_data)
+	if m:
+		name = utility.unescape(m.group(1))
+	else:
+		return "Hmm, are you sure the course code is " + code + "?"
+	
+	# Locate the English course name
+	m = re.search(
+		"\<br\>/(.*?)/\</b\>\</span\>",
+		sh_data)
+	if m:
+		name = name + " (" + utility.unescape(m.group(1)) + ")"
+	else:
+		print "I couldn't find the English name of the LiTH course " + \
+			code + " O.o"
+	
+	# Locate the number of HP (ECTS) credits
+	m = re.search(
+		"\<span class=\"txtbold\"\>\<b\> (\d{1,2},?\d?) hp\</span\>\</font\>",
+		sh_data)
+	if m:
+		credits = m.group(1).replace(",", ".")
+	else:
+		credits = "???"
+		#print "I couldn't find the number of credits for the LiTH course " + \
+		#	code + " O.o"
+	
+	# Locate the advancement level
+	m = re.search(
+		"\<span class=\"txtkursivlista\"\>Utbildningsniv&aring; \(G1,G2,A\):\<\/span\>\<i\> \<\/i\>\<span class=\"txtlista\"\>(.+?)\<\/span\>",
+		sh_data)
+	if m:
+		level = m.group(1)
+	else:
+		level = "???"
+	
+	# Fetch the schedule page for the course from the study handbook
+	response = utility.read_url(schedule_url(code, programme, year))
+	sh_data = response["data"]
+	
+	# Match study periods
+	# (Usually ([HV]t[12]) but some odd courses have other formats, e.g. Ht2a)
+	period_m = re.findall(
+		"\<td\>\<span class=\"txtlista\"\>\d([HV]t.*?)\</span\>\</td\>",
+		sh_data)
+	
+	# Match blocks
+	# (Usually ([1-4]) but some courses have other formats, e.g. "-", "" or "1+2+4")
+	block_m = re.findall(
+		"&-Token.ksk=\[Field:'k_kurskod'\]\"\>---\>" + "(.*?)" + \
+		"\<!---\<\/a\>---\>\<\/span\>\<\/td\>",
+		sh_data)
+	
+	# Build a list of schedule occasions
+	schedules = []
+	for i in range(len(period_m)):
+		# Assemble an occasion string
+		match = period_m[i] + "."
+		if not block_m[i]:
+			match += "?"
+		else:
+			match += block_m[i].replace(", ", "+")
+		
+		# Add if not already present
+		if match not in schedules:
+			schedules.append(match)
+	
+	# Convert it into a string
+	if schedules:
+		schedule_text = "Scheduled during " + ", ".join(sorted(schedules))
+	else:
+		schedule_text = "Not scheduled " + year + "."
+	
+	# Combine all the information and return it
+	return code + ": " + name + ", " + credits + " HP on level " + level + \
+			". " + schedule_text + " | " + sh_url(code, year)
+
 class LithCourse(Command):
 	def __init__(self):
 		pass
@@ -74,77 +159,5 @@ class LithCourse(Command):
 		if not year:
 			year = sh_year()
 		
-		# Fetch the study handbook page for the course
-		response = utility.read_url(sh_url(code, year))
-		sh_data = response["data"]
-		
-		# Locate the Swedish course name
-		m = re.search(
-			"\<span class=\"txtbold\"\>\<b\>(.*?), \d{1,2},?\d? p \</b\>",
-			sh_data)
-		if m:
-			name = utility.unescape(m.group(1))
-		else:
-			return "Hmm, are you sure the course code is " + code + "?"
-		
-		# Locate the English course name
-		m = re.search(
-			"\<br\>/(.*?)/\</b\>\</span\>",
-			sh_data)
-		if m:
-			name = name + " (" + utility.unescape(m.group(1)) + ")"
-		else:
-			print "I couldn't find the English name of the LiTH course " + \
-				code + " O.o"
-		
-		# Locate the number of HP (ECTS) credits
-		m = re.search(
-			"\<span class=\"txtbold\"\>\<b\> (\d{1,2},?\d?) hp\</span\>\</font\>",
-			sh_data)
-		if m:
-			credits = m.group(1).replace(",", ".")
-		else:
-			credits = "???"
-			print "I couldn't find the number of credits for the LiTH course " + \
-				code + " O.o"
-		
-		# Fetch the schedule page for the course from the study handbook
-		response = utility.read_url(schedule_url(code, programme, year))
-		sh_data = response["data"]
-		
-		# Match study periods
-		# (Usually ([HV]t[12]) but some odd courses have other formats, e.g. Ht2a)
-		period_m = re.findall(
-			"\<td\>\<span class=\"txtlista\"\>\d([HV]t.*?)\</span\>\</td\>",
-			sh_data)
-		
-		# Match blocks
-		# (Usually ([1-4]) but some courses have other formats, e.g. "-", "" or "1+2+4")
-		block_m = re.findall(
-			"&-Token.ksk=\[Field:'k_kurskod'\]\"\>---\>" + "(.*?)" + \
-			"\<!---\<\/a\>---\>\<\/span\>\<\/td\>",
-			sh_data)
-		
-		# Build a list of schedule occasions
-		schedules = []
-		for i in range(len(period_m)):
-			# Assemble an occasion string
-			match = period_m[i] + "."
-			if not block_m[i]:
-				match += "?"
-			else:
-				match += block_m[i]
-			# Add if not already present
-			if match not in schedules:
-				schedules.append(match)
-		
-		# Convert it into a string
-		if schedules:
-			schedule_text = "Scheduled during " + ", ".join(sorted(schedules))
-		else:
-			schedule_text = "Not scheduled " + year + "."
-		
-		# Combine all the information and return it
-		return code + ": " + name + ", " + credits + " HP. " + schedule_text + \
-				" | " + sh_url(code, year)
+		return lith_course_info(code, programme, year)
 
