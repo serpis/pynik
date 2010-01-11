18c064b0d07f79c1b55ecfcaea2d66ff466dd299
merola <merola@9307e528-e534-0410-a844-a96db09f00f2>
2010-01-11 00:01:50 +0000

    FFML - FML in Swedish
    git-svn-id: http://stalverk80.se/svn/pynik@247 9307e528-e534-0410-a844-a96db09f00f2

--- plugins/fml.py
+++ plugins/fml.py
@@ -17,37 +17,52 @@ def get_node_text(node):
 	
 	return text
 
-def get_fml_dom(identifier):
+def get_fml_dom(identifier, lang):
 	url = "http://api.betacie.com/view/" + identifier + \
-			"/nocomment?key=readonly&language=en"
+			"/nocomment?key=readonly&language=" + lang
 	response = utility.read_url(url)
 	data = response["data"]
 	return minidom.parseString(data)
 
-def fml_entry(identifier):
-	dom = get_fml_dom(identifier)
+def base_fml_url(lang):
+	if lang = "se":
+		return "http://www.fanformittliv.com/"
+	else:
+		return "http://www.fmylife.com/"
+
+def fml_entry(identifier, lang):
+	dom = get_fml_dom(identifier, lang)
+	result = ""
 	
 	if dom.getElementsByTagName("code")[0] == "0":
-		return "Today, I could not use the API. FML | http://www.fmylife.com/"
+		if lang = "se":
+			result = "Idag lyckades jag inte använda API:et. FFML"
+		else:
+			result =  "Today, I could not use the API. FML"
+		return result + " | " + base_fml_url(lang)
 	
 	items = dom.getElementsByTagName("items")[0].getElementsByTagName("item")
 	
 	if not items:
-		return "Today, I could not find a result. FML | http://www.fmylife.com/"
+		if lang = "se":
+			result = "Idag lyckades jag inte hitta något resultat. FFML"
+		else:
+			result = "Today, I could not find a result. FML"
+		return result + " | " + base_fml_url(lang)
 	
 	item = items[0]
 	
 	return get_node_text(item.getElementsByTagName("text")[0]) + " | " + \
 			"+" + get_node_text(item.getElementsByTagName("agree")[0]) + " / " + \
 			"-" + get_node_text(item.getElementsByTagName("deserved")[0]) + \
-			" | " + "http://www.fmylife.com/" + \
+			" | " + base_fml_url(lang) + \
 			get_node_text(item.getElementsByTagName("category")[0]) + "/" + \
 			item.getAttribute("id")
 
 class FMLCommand(Command):
 	types = ['last', 'random', 'top', 'top_day', 'top_week', 'top_month', 'flop',
-			  'flop_day', 'flop_week', 'flop_month', 'love', 'money', 'kids',
-			  'work', 'health', 'sex', 'miscellaneous']
+			'flop_day', 'flop_week', 'flop_month', 'love', 'money', 'kids',
+			'work', 'health', 'sex', 'miscellaneous']
 	
 	usage = "Usage: .fml <type> | .fml <id> | .fml show_types"
 	
@@ -70,9 +85,47 @@ class FMLCommand(Command):
 			
 			# If the argument is a valid type, look it up
 			elif (argument in self.types) or (argument.isdigit()):
-				result = fml_entry(argument)
+				result = fml_entry(argument, "en")
 			elif m:
-				result = fml_entry(m.group(1))
+				result = fml_entry(m.group(1), "en")
+			
+			if result:
+				# Return result (encoded to make IRCClient.send() happy)
+				return result.encode('utf-8', 'replace')
+		
+		# Non-valid argument
+		return self.usage
+
+class FFMLCommand(Command):
+	types = ['last', 'random', 'top', 'top_day', 'top_week', 'top_month', 'flop',
+			'flop_day', 'flop_week', 'flop_month', 'karlek', 'pengar', 'barn',
+			'arbete', 'halsa', 'sex', 'annat']
+	
+	usage = "Användning: .fml <typ> | .fml <id> | .fml visa_typer"
+	
+	def __init__(self):
+		pass
+		
+	def trig_ffml(self, bot, source, target, trigger, argument):
+		"""Kommando för att visa anekdoter från www.fanformittliv.com"""
+		
+		# Sanitize argument
+		argument = argument.strip().lower()
+		argument = utility.asciilize(argument)
+		
+		if argument:
+			m = re.match("http:\/\/www\.fanformittliv\.com\/\w+\/(\d+)", argument)
+			result = ""
+			
+			# Show all types, if requested
+			if argument == 'visa_typer':
+				result = "Tillgängliga typer: " + ", ".join(self.types)
+			
+			# If the argument is a valid type, look it up
+			elif (argument in self.types) or (argument.isdigit()):
+				result = fml_entry(argument, "se")
+			elif m:
+				result = fml_entry(m.group(1), "se")
 			
 			if result:
 				# Return result (encoded to make IRCClient.send() happy)
