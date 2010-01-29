b944562853d787da18b00f8d9cd532d82f0055cc
IcEBnd <IcEBnd@github>
2010-01-30 00:53:46 +0100

    Added .code command for information about github repo

---
+++ plugins/code.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+from commands import Command
+
+class Code(Command):
+	def __init__(self):
+		pass
+	
+	def trig_code(self, bot, source, target, trigger, argument):
+		""" I'm sorry Dave I can't do that. """
+		return "Hello, I'm riverbot, a pyirkbot based on pynik. My code http://github.com/IcEBnd/pyirkbot For feature requests use http://github.com/IcEBnd/pyirkbot/issues"
+
+	def save(self): 
+		pass
+
+	def on_load(self): 
+		pass
+
+	def on_unload(self): 
+		pass
