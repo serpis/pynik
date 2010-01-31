d1ce79817d516353a57eb0c5333397b73fe56330
Merola <martin.erola@gmail.com>
2010-01-31 13:48:14 +0100

    Adding timestamp to error messages.

--- error_handler.py
+++ error_handler.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import time
 
-def output_message(message, severity=5):
+def output_message(message, severity=3):
 	# Maybe it would be nice to output in different formats depending on
 	# how severe the error is. Not implemented yet though.
 	
+	string = time.strftime("[%H:%M:%S] ")
+	
 	try:
-		# For now, just send everything to stderr.
-		print >> sys.stderr, message
+		string += str(message)
 	except:
-		print >> sys.stderr, "Unknown error occured, non-printable message sent. Shouldn't happen :S"
+		string += "Unknown error occured, non-printable message sent. Shouldn't happen :S"
+	
+	# For now, just send everything to stderr.
+	print >> sys.stderr, string
 
