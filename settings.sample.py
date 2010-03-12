bf0ad75eefebb4ccc96f3984f98acbf158819be7
IcEBnd <IcEBnd@github>
2010-03-13 00:55:11 +0100

    Added most of the plugins to the sample config.

--- settings.sample.py
+++ settings.sample.py
@@ -21,5 +21,19 @@ class Settings(AutoReloader):                      # these two lines
 	# Plugins that will be loaded on startup from plugins/
 	# Use this directory to view all available Plugins and to add your own.
 	plugins = ['plugins', 'command_catcher', 'commands', 'standard',
-			   'reloader', 'options', 'utility']
+		   'reloader', 'options', 'utility']
+
+	# If you want to enable more plugins comment out the above
+	# and uncomment the ones below.
+	#plugins = ['plugins', 'command_catcher', 'commands', 'standard',
+	#	   'reloader', 'options', 'utility',
+	#	   "aduno", "alpha", "char", "code", "compliment", "countdown", 
+	#	   "down", "example_plugin", "favorites", "festern_bbq", "fml", 
+	#	   "food", "game_plugin", "give", "googlefight", "ical_parser", 
+	#	   "icq", "imdb", "isitfriday", "kolli", "lithcourse", "magic8ball", 
+	#	   "mat", "metacritic", "nelson", "nextep", "notes", "pi", 
+	#	   "postnr", "prisjakt", "pylisp", "qotd", "randombuy", 
+	#	   "reminder", "roulette", "rss", "scale", "spotify", "stava", 
+	#	   "systembolaget", "tenta", "timezone", "title_reader", "tv", 
+	#	   "tyda", "yrno"]
 
