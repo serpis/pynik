f9aabb351f5258579794fc3320dd1c2d455c2f52
Patrik Ragnarsson <patrik@starkast.net>
2011-09-19 16:37:40 +0200

    Removed dependency to python-twitter

--- plugins/tweet.py
+++ plugins/tweet.py
@@ -1,21 +1,39 @@
-#-*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 import re
 import sys
-import twitter
+import utility
+from json import JSONDecoder
 from commands import Command
 
 # TODO:
 # - add caching
+# - print out error messages
+# - rate limiting (only allowed 150 requests per hour to Twitter API)
+# - get screen_name from API instead of URL, can be incorrect in URL
 
 class Tweet:
 	idno = ''
 	user = ''
 	text = ''
+	erro = ''
 
 def get_tweet_text(idno):
-	api = twitter.Api()
-	status = api.GetStatus(idno)
-	return status.GetText()
+	decoder = JSONDecoder()
+	url = "https://api.twitter.com/1/statuses/show/" + idno + ".json"
+	response = utility.read_url(url)
+
+	if not response:
+		# Couldn't connect to Twitter API
+		return False
+
+	try:
+		data = decoder.decode(response['data'])
+	except Exception:
+		# Couldn't parse the API output
+		return False
+
+	# Use latin-1 to make IRCClient.send() happy
+	return data.get(u"text").encode('latin-1', 'replace')
 
 def get_tweet(message):
 	regexp = 'https://twitter.com/#!/(\w+)/status/(\d+)'
@@ -25,7 +43,10 @@ def get_tweet(message):
 		tweet.user = m.group(1)
 		tweet.idno = m.group(2)
 		tweet.text = get_tweet_text(tweet.idno)
-		return tweet
+		if tweet.text:
+			return tweet
+		else:
+			return False
 	else:
 		return False
 
