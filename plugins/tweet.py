a98b3c60eb0e3fcd67046969c68b69ca18855851
Patrik Ragnarsson <patrik@starkast.net>
2011-09-27 02:55:43 +0200

    Added support for Twitter URLs without #!/

--- plugins/tweet.py
+++ plugins/tweet.py
@@ -17,9 +17,9 @@ class Tweet:
 	text = ''
 	erro = ''
 
-def get_tweet_text(idno):
+def get_tweet_text_and_user(tweet):
 	decoder = JSONDecoder()
-	url = "https://api.twitter.com/1/statuses/show/" + idno + ".json"
+	url = "https://api.twitter.com/1/statuses/show/" + tweet.idno + ".json"
 	response = utility.read_url(url)
 
 	if not response:
@@ -30,20 +30,23 @@ def get_tweet_text(idno):
 		data = decoder.decode(response['data'])
 	except Exception:
 		# Couldn't parse the API output
+		print("Couldn't parse the API output")
 		return False
 
 	# Use latin-1 to make IRCClient.send() happy
-	return data.get(u"text").encode('latin-1', 'replace')
+	tweet.text = data.get(u"text").encode('latin-1', 'replace')
+	tweet.user = data.get(u"user").get(u"screen_name").encode('latin-1', 'replace')
+	return tweet
 
 def get_tweet(message):
-	regexp = 'https://twitter.com/#!/(\w+)/status/(\d+)'
+	# https://twitter.com/judofyr/status/118364541787709440
+	regexp = 'https://twitter.com/((#!/(\w+))|(\w+))/status/(\d+)'
 	m = re.search(regexp, message, re.IGNORECASE)
 	if m:
 		tweet = Tweet()
-		tweet.user = m.group(1)
-		tweet.idno = m.group(2)
-		tweet.text = get_tweet_text(tweet.idno)
-		if tweet.text:
+		tweet.idno = m.group(5)
+		tweet = get_tweet_text_and_user(tweet)
+		if tweet:
 			return tweet
 		else:
 			return False
