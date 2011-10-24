d2a19fb093b2ea6a96d801cf4338e77ff33f788d
Patrik Ragnarsson <patrik@starkast.net>
2011-10-25 00:39:26 +0200

    Disable GitHub plugin (uses simplejson)

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -6,5 +6,4 @@ __all__ = ['plugins', 'command_catcher', 'first_plugin', 'auto_join', 'aduno',
             'ical_parser', 'icq', 'pylisp', 'lithcourse', 'scale', 'postnr',
             'tenta', 'prisjakt', 'spotify', 'stava', 'down', 'metacritic',
             'notes', 'fml', 'systembolaget', 'randombuy', 'festern_bbq',
-            'compliment', 'roulette', 'tyda', 'char', 'tweet', 'erekebabfredag',
-            'github']
+            'compliment', 'roulette', 'tyda', 'char', 'tweet', 'erekebabfredag']
