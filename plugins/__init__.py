263feaaf4eafd0a3bfcdbfe9bc4a7249976edb9d
zppln <zeppelin@fuckedforlife.org>
2014-04-18 18:41:17 +0200

    Added a small plugin for getting stock data

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4,6 +4,6 @@ __all__ = ['plugins', 'command_catcher', 'first_plugin', 'auto_join', 'aduno',
             'example_command', 'rss', 'googlefight', 'teewars', 'tw_nazi',
             'give', 'reminder', 'kolli', 'imdb', 'mat', 'timezone',
             'ical_parser', 'icq', 'pylisp', 'lithcourse', 'scale', 'postnr',
-            'tenta', 'prisjakt', 'spotify', 'stava', 'down', 'metacritic',
+            'tenta', 'prisjakt', 'spotify', 'stava', 'stock', 'down', 'metacritic',
             'notes', 'fml', 'systembolaget', 'randombuy', 'festern_bbq',
             'compliment', 'roulette', 'tyda', 'yrno', 'tweet', 'calc']
---
+++ plugins/stock.py
@@ -0,0 +1,53 @@
+from commands import Command
+
+from urllib2 import Request, urlopen
+
+""" mIRC style color codes """
+GREEN = "\x033"
+RED = "\x034"   
+ENDC = "\x03"
+
+class StockCommand(Command):
+    usage = "Usage: .stock <ticker symbol>"
+
+    def __init__(self):
+        pass
+
+    def __get(self, ticker_symbol):
+        """ Requests a CSV file with the data from Yahoo Finance """
+
+        # n = name
+        # b2 = ask price
+        # c1 = change
+        # p2 = change in percent
+        url = "http://finance.yahoo.com/d/quotes.csv?s=%s&f=nb2c1p2" % ticker_symbol
+        req = Request(url)
+        resp = urlopen(req) 
+        csv_str = resp.read().decode().strip()
+
+        elems = csv_str.split(',')
+
+        return dict(name=elems[0].strip('"'), ask_price=elems[1], change=elems[2], changep=elems[3].strip('"'))
+
+    def trig_stock(self, bot, source, target, trigger, argument):
+        """ Retreives some stock data for a given ticker symbol """
+
+        ticker_symbol = argument.strip().split(' ', 1)[0]
+        if not ticker_symbol:
+            return self.usage
+
+        try:
+            data = self.__get(ticker_symbol)
+        except:
+            return "Couldn't get data from Yahoo Finance! Sorry!"
+
+        if data["change"].startswith('+'):
+            return data["name"] + " " + data["ask_price"] + " " + \
+                   GREEN + data["change"] + " " + data["changep"] + ENDC
+        elif data["change"].startswith('-'):
+            return data["name"] + " " + data["ask_price"] + " " + \
+                   RED + data["change"] + " " + data["changep"] + ENDC
+        else:
+            return data["name"] + " " + data["ask_price"] + " " + \
+                   data["change"] + " " + data["changep"] 
+
